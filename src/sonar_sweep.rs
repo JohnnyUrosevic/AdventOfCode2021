35a4444fcb2a946b58ae52135e9eeeaa0b689d37
JohnnyUrosevic <johnnyurosevic@g.ucla.edu>
2021-12-09 03:34:49 -0800

    Day 1 Part 2

--- src/sonar_sweep.rs
+++ src/sonar_sweep.rs
@@ -4,12 +4,24 @@ pub fn sonar_sweep() -> (i32, i32) {
     let input = get_input(1).expect("Could not get input");
 	let parsed: Vec<i32> = input.iter().map(|e| e.parse::<i32>().unwrap()).collect();
 
-    let sequence = parsed.iter().zip(parsed.iter().skip(1));
-
-    let count = sequence.fold(0, |acc, e| {
+	let compare = |acc, e| {
 		let (a, b) = e;
 		acc + (a < b) as i32
-    });
+    };
 
-    (count, 0)
+    let sequence = parsed.iter().zip(parsed.iter().skip(1));
+
+    let count = sequence.fold(0, compare);
+
+	let triplets = parsed.iter().zip(parsed.iter().skip(1).zip(parsed.iter().skip(2)));
+    let summed: Vec<i32> = triplets.map(|e| {
+		let (a, (b, c)) = e;
+		a + b + c
+	}).collect();
+
+	let summed_sequence = summed.iter().zip(summed.iter().skip(1));
+	
+	let count2 = summed_sequence.fold(0, compare);
+
+    (count, count2)
 }
