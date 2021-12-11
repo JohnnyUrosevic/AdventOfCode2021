ed417a99156570e743081454fddfb6f4f776cd4f
Johnny Urosevic <themagicalcake@gmail.com>
2021-12-11 02:50:46 -0800

    Day 4: Bingo part 1

---
+++ src/bingo.rs
@@ -0,0 +1,57 @@
+use crate::get_input::get_input;
+
+use std::collections::HashMap;
+
+pub fn bingo() -> (i32, i32) {
+    let input = get_input(4).expect("Could not get input");
+    let mut input_iter = input.iter();
+    
+    let called_numbers = input_iter.next()
+        .expect("Bad input")
+        .split(',')
+        .map(|e| e.parse::<i32>().expect("NaN"));
+
+    let mut boards: Vec<HashMap<i32, (usize, usize)>> = vec![];
+    let mut row: usize = 0;
+    let mut boards_len = 0;
+
+    for line in input_iter {
+        if line.is_empty() {
+            row = 0;
+            boards.push(HashMap::new());
+            boards_len += 1;
+        }
+        else {
+            for (col, num) in line.split(' ').filter(|e| !e.is_empty()).enumerate() {
+                boards[boards_len-1]
+                    .insert(num.parse().expect("NaN"), (row, col));
+            }
+            row += 1;
+        }
+    }
+
+    let mut row_counts = vec![vec![0; 5]; boards_len];
+    let mut col_counts= vec![vec![0; 5]; boards_len];
+
+    let mut score = 0;
+
+    'outed: for called in called_numbers {
+        for (i, board) in boards.iter_mut().enumerate() {
+            let (r, c) = match board.remove(&called) {
+                Some((r, c)) => (r, c),
+                None => continue,
+            };
+
+            row_counts[i][r] += 1;
+            col_counts[i][c] += 1;
+
+            if row_counts[i][r] == 5 || col_counts[i][c] == 5 {
+                let unmarked: i32 = board.keys().sum();
+                score = unmarked * called;
+                break 'outed;
+            }
+        }
+    }
+
+    (score, 0)
+}
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ mod get_input;
 mod sonar_sweep;
 mod dive;
 mod binary_diagnostic;
+mod bingo;
 
 use sonar_sweep::sonar_sweep;
 use dive::dive;
 use binary_diagnostic::binary_diagnostic;
+use bingo::bingo;
 
 use std::fmt::Display;
 
@@ -19,4 +21,5 @@ fn main() {
     print_results(1, sonar_sweep());
     print_results(2, dive());
     print_results(3, binary_diagnostic());
+    print_results(4, bingo());
 }
